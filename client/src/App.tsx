18d768e4b5d119e3f94445920590d7b0283bd1f3
vipin vishwakarma <vishwakarmavipin3434@gmail.com>
2026-06-16 11:37:05 +0530

    add loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,12 +11,15 @@ import RankTracker from "./pages/RankTracker";
 import RankDetail from "./pages/RankDetail";
 import { Toaster } from "react-hot-toast";
 import { useApp } from "./context/AppContext";
+import Loading from "./components/Loading";
 
 export default function App() {
   const { user, loading } = useApp();
   const location = useLocation();
 
   const hideNavbar = ["/login", "/register"].includes(location.pathname);
+
+  if (loading) <Loading />;
 
   return (
     <>
